d8e941f50c31caf4fc131ffb52e1264cdc05460a
Kevin <kevinbarra2001@gmail.com>
2025-11-22 03:05:05 -0600

    Agregando Analíticas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,17 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
+import { Analytics } from "@vercel/analytics/react"; // <--- IMPORTACIÓN NUEVA
 
 const inter = Inter({ subsets: ["latin"] });
 
-// --- AQUÍ ESTÁ EL CAMBIO DE SEO ---
 export const metadata: Metadata = {
   title: "Kevin Consulting | Transformación Digital para Negocios",
   description: "Ingeniería de software a la medida. Optimizamos inventarios, procesos y ventas para panaderías, restaurantes y contratistas.",
   keywords: ["Consultoría de Software", "Desarrollo Web", "Inventarios", "Puebla", "Transformación Digital"],
   authors: [{ name: "Kevin Consulting" }],
   icons: {
-    icon: '/icon', // Apunta al archivo que acabamos de crear
+    icon: '/icon',
   },
 };
 
@@ -26,6 +26,9 @@ export default function RootLayout({
       <body className={`${inter.className} bg-slate-950 text-white antialiased`}>
         <Navbar />
         {children}
+        
+        {/* SENSOR DE ANALÍTICAS (Invisible pero poderoso) */}
+        <Analytics />
       </body>
     </html>
   );
